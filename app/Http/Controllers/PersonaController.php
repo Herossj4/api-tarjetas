40db06a1444c1533252da8206fb81536a65ef684
Jeison <davidbuitrago1996@hotmail.com>
2023-06-06 10:13:39 -0500

    Cambios hasta el 6/06/2023

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -9,7 +9,23 @@ use Illuminate\Support\Facades\DB;
 class PersonaController extends Controller
 {
     public function getPersonas() {
-        $datos = DB::select("select * from tb_personas order by persona_id");
+        $datos = DB::select("select p.persona_id,
+        p.numero_identificacion,
+        p.grado,
+        p.nombres,
+        p.apellidos,
+        p.imagen,
+        p.unidad,
+        (select u.nombre_unidad from tb_unidades u where u.unidad_id = p.unidad) nombre_unidad,
+        p.dependencia,
+        (select u.nombre_unidad from tb_unidades u where u.unidad_id = p.dependencia) nombre_dependencia,
+        p.cargo,
+        p.usuario_creador,
+        p.fecha_creacion,
+        p.usuario_modificador,
+        p.fecha_modificacion,
+        p.tipo_persona 
+    from tb_personas p;");
 
         $response = json_encode(array('result' => $datos, 'tipo' => 0), JSON_NUMERIC_CHECK);
         $response = json_decode($response);
@@ -21,7 +37,7 @@ class PersonaController extends Controller
         $m = new Persona();
         $persona = $m->crud_personas($request, 'C');
         if ($persona->persona_id != 0) {
-            $response = json_encode(array('mensaje' => 'Ha creado exitosamente.', 'tipo' => 0, 'id' => $persona->persona_id), JSON_NUMERIC_CHECK);
+            $response = json_encode(array('mensaje' => 'Ha creado exitosamente.', 'tipo' => 0, 'id' => (int)$persona->persona_id), JSON_NUMERIC_CHECK);
             $response = json_decode($response);
 
             return response()->json($response);
